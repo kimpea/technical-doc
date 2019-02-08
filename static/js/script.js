5b40db7deaf9f667487634fb18d0f75c75278f19
Kim Pearton <40198674+kimpea@users.noreply.github.com>
2019-02-08 11:08:06 +0000

    Add function for back-to-top button

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,17 @@
-$(document).ready(function () {
-    $('.button-collapse').sideNav({
-        menuWidth: 300, // Default is 300
-        edge: 'left', // Choose the horizontal origin
-        closeOnClick: false, // Closes side-nav on <a> clicks
-        draggable: true // Choose whether you can drag to open on touch screens,
-    });
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("back-to-top").style.display = "block";
+    }
+    else {
+        document.getElementById("back-to-top").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
